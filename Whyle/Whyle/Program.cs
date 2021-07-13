a1be5cda3e50886306500394788a9f8a12b321a1
rodrigopastorfelis <84048157+rodrigopastorfelis@users.noreply.github.com>
2021-07-13 14:38:18 -0300

    Add files via upload

---
+++ Whyle/Whyle/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Whyle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int soma=0;
+            Console.WriteLine("Digite os valores das vendas, quando quiser cancelar digite o valor como 0");
+            int n1  = int.Parse(Console.ReadLine());
+            while (n1 != 0)
+            {
+                soma += n1;
+                n1 = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(soma);
+        }
+    }
+}
